21ea8e93d569847b26a73e5e61e1daafdfafe18f
Sergei CZigarkin <chigarkin.s.s@edu.mirea.ru>
2021-02-12 02:18:54 +0300

    ITC#

---
+++ PROVERKA/PROVERKA/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PROVERKA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
