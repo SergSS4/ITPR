fca0b0d4cee3f55d30098c64b9b18befa01ce909
Sergei CZigarkin <chigarkin.s.s@edu.mirea.ru>
2021-02-12 02:39:21 +0300

    ITC#

---
+++ PR01/PR03/PR03/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PR03
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = textBox1.Text;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            this.BackColor = Color.FromArgb(random.Next(255), random.Next(255), random.Next(255));
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Начало работы";
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            this.BackColor = Color.FromArgb(random.Next(255), random.Next(255), random.Next(255));
+            label1.Text = "Начало работы";
+            textBox1.Text = "";
+        }
+    }
+}
