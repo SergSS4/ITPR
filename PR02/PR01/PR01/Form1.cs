21d943260354a7cfbfcf36b1753183bde235cc51
Sergei CZigarkin <chigarkin.s.s@edu.mirea.ru>
2021-02-12 18:37:16 +0300

    ITC#

---
+++ PR02/PR01/PR01/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PR01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = "2";
+            textBox2.Text = "Практическая работа №2 Чигаркин С.С.";
+            textBox2.Text += Environment.NewLine + "Рассчитать значение выражения y=((sqrt(1+(e^(sqrt(x))+cos(x^(2)))/|1-sin^(3)*x|)+log(|2x|)";
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double x = double.Parse(textBox1.Text);
+            textBox2.Text += Environment.NewLine + "При x= " + x.ToString();
+            double y = ((Math.Sqrt(1 + Math.Pow(Math.E, Math.Sqrt(x))) + Math.Cos(Math.Pow(x, 2))) / Math.Abs(1 - Math.Pow(Math.Sin(x), 2) * x)) + Math.Log(Math.Abs(2 * x));
+            textBox2.Text += Environment.NewLine + "Результат y = " + y.ToString();
+        }
+    }
+}
